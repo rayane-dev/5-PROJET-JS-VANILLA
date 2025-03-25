7764a64c1a8ed9aab2cc983ad3f4b5d9646963a6
rayane-dev <95442692+rayane-dev@users.noreply.github.com>
2025-03-25 12:10:25 +0100

    Mise à jour horloge (sans responsive)

--- Horloge/script.js
+++ Horloge/script.js
@@ -11,7 +11,6 @@ console.log(time[2]);
 
 
 
-
 setInterval(mafonction,1000);
 
 function mafonction(){
@@ -27,12 +26,99 @@ else{
 }
 
  
-    
- // console.log(now.getHours(), now.getMinutes(), now.getSeconds())
 }
 
 
+function intializeClock(){
+    const nyDateStr = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
+    const londonDateStr = new Date().toLocaleString('en-BR', { timeZone: 'Europe/London' });
+
+
+    let now = [new Date(), new Date(nyDateStr), new Date(londonDateStr)];
+    hours = [now[0].getHours(), now[1].getHours(), now[2].getHours()];
+    minute = [now[0].getMinutes(), now[1].getMinutes(), now[2].getMinutes()];
+    second = [now[0].getSeconds(), now[1].getSeconds(), now[2].getSeconds()];
+
+
+    let hourspointer = document.querySelectorAll('.hours-container');
+    let minutespointer = document.querySelectorAll('.minutes-container');
+    let secondspointer = document.querySelectorAll('.seconds-container');
+
+   
+//  //HORRAIRE PARIS
+    hourspointer[0].style.transform = 'rotateZ(' + hours[0] * 30 + 'deg)';
+    minutespointer[0].style.transform = 'rotateZ(' + minute[0] * 6 + 'deg)';
+    secondspointer[0].style.transform = 'rotateZ(' + second[0] * 6 + 'deg)'; ;
+   
+    
+
+   //HORRAIRE NEW YORK
+    hourspointer[1].style.transform = 'rotateZ(' + hours[1] * 30 + 'deg)';
+    secondspointer[1].style.transform = 'rotateZ(' + second[1] * 6 + 'deg)';
+    minutespointer[1].style.transform = 'rotateZ(' + minute[1] * 6 + 'deg)';
+
+    //HORRAIRE LONDRE
+    hourspointer[2].style.transform = 'rotateZ(' + hours[2] * 30 + 'deg)';
+    secondspointer[2].style.transform = 'rotateZ(' + second[2] * 6 + 'deg)';
+    minutespointer[2].style.transform = 'rotateZ(' + minute[2] * 6 + 'deg)';
+   
+    
+}
+
+
+setInterval(intializeClock, 1000);
+
+
+// //NEW YORK
+// function NewYorkClock(){
+//     const nyDateStr = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
+//     const nyDate = new Date(nyDateStr);
+//     console.log(nyDate);
+
+    
+//     hours = nyDate.getHours();
+//     minute = nyDate.getMinutes();
+//     second = nyDate.getSeconds();
+
+//    let hourspointer = document.querySelectorAll('.hours-container');
+//    let minutespointer = document.querySelectorAll('.minutes-container');
+//    let secondspointer = document.querySelectorAll('.seconds-container');
+
+//     //HORRAIRE NEW YORK
+//     hourspointer[1].style.transform = 'rotateZ(' + hours * 30 + 'deg)';
+//     minutespointer[1].style.transform = 'rotateZ(' + minute * 6 + 'deg)';
+//     secondspointer[1].style.transform = 'rotateZ(' + second * 6 + 'deg)'; 
+    
+    
+// }
+
+
+// setInterval(NewYorkClock, 1000);
 
 
 
+// //LONDRE
+// function londonClock(){
+//     const londonDateStr = new Date().toLocaleString('en-BR', { timeZone: 'Europe/London' });
+//     const londonDate = new Date(londonDateStr);
+//     console.log(nyDate);
 
+    
+//     hours = nyDate.getHours();
+//     minute = nyDate.getMinutes();
+//     second = nyDate.getSeconds();
+
+//    let hourspointer = document.querySelectorAll('.hours-container');
+//    let minutespointer = document.querySelectorAll('.minutes-container');
+//    let secondspointer = document.querySelectorAll('.seconds-container');
+
+//     //HORRAIRE NEW YORK
+//     hourspointer[1].style.transform = 'rotateZ(' + hours * 30 + 'deg)';
+//     minutespointer[1].style.transform = 'rotateZ(' + minute * 6 + 'deg)';
+//     secondspointer[1].style.transform = 'rotateZ(' + second * 6 + 'deg)'; 
+    
+    
+// }
+
+
+// setInterval(londonClock(), 1000);
